8bdd8615cc7840f16638f6a45f0349e3912fcc30
Joshua Friede <7959266+joshua-friede@users.noreply.github.com>
2021-01-01 23:06:47 -0600

    add site metadata, manifest, eslint, helmet, offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,31 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Joshua Friede',
+    description: 'Joshua Friede is a software developer based in Austin, Texas ',
+    author: 'Joshua Friede',
+  },
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: 'src/utils/typography',
       },
     },
-    'gatsby-plugin-eslint'
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Joshua Friede',
+        short_name: 'JoshuaFriede',
+        start_url: '/',
+        lang: 'en',
+        background_color: '#fff',
+        theme_color: '#6b37bf',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
   ],
-}
+};
