71a8e5c25a72d46d2744a61b9eb0b1dd18e8040e
Joshua Friede <7959266+joshua-friede@users.noreply.github.com>
2021-01-06 19:57:13 -0600

    style nav with flex box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,27 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ siteMetadata }) => {
   return (
     <header className="mb-12">
-      <h3 className="inline text-2xl font-semibold">{siteMetadata.title}</h3>
-      <nav className="float-right">
-        <Link className={classes.navLink} to="/">
-          Home
-        </Link>
-        <Link className={classes.navLink} to="/about/">
-          About
-        </Link>
-        <Link className={classes.navLink} to="/contact/">
-          Contact
-        </Link>
+      <nav className="flex items-center justify-between">
+        <h3 className="text-2xl font-semibold inline-block">
+          {siteMetadata.title}
+        </h3>
+        <ul className="flex">
+          <li className="inline-block px-5 py-4">
+            <Link className={classes.navLink} to="/">
+              Home
+            </Link>
+          </li>
+          <li className="inline-block px-5 py-4">
+            <Link className={classes.navLink} to="/about/">
+              About
+            </Link>
+          </li>
+          <li className="inline-block px-5 py-4">
+            <Link className={classes.navLink} to="/contact/">
+              Contact
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
@@ -26,7 +36,7 @@ const Header: React.FC<HeaderProps> = ({ siteMetadata }) => {
 
 const classes = {
   title: "inline text-2xl font-semibold",
-  navLink: "mr-6 text-green-700 underline",
+  navLink: "text-green-700 underline",
 };
 
 export default Header;
